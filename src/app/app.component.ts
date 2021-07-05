099b5a08661e5f2bf4f52108a6ccf70aa7d9221f
unknown <andersonlantunes@gmail.com>
2021-07-05 19:32:43 -0300

    Teste http request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-tour-of-heroes';
+  address: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get('https://viacep.com.br/ws/01001000/json/')
+      .subscribe((res) => {
+        console.log(res);
+        this.address = res;
+      });
+  }
 }
